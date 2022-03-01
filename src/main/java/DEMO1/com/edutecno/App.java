7d70b778dd2ccb76c9a80c0e948b3972ad02c988
adrian <adrian@LAPTOP-D6Q9IDB6>
2022-03-01 11:52:06 -0300

    Merge branch 'main' of https://github.com/adrianedutecno/prueba.git into main

--- src/main/java/DEMO1/com/edutecno/App.java
+++ src/main/java/DEMO1/com/edutecno/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Cambios realizados");
+        System.out.println("se siguen realizando cambios");
     }
 }
